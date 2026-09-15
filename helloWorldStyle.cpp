17d64697236cc281980dd06c55c71cebb468b6d6
Lennon Cehajic <lennon.cehajic@stu.ocsb.ca>
2026-09-15 12:43:02 +0000

    Added helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2026 Lennon Cehajic All rights reserved.
+
+// .
+
+// Created by: Lennon Cehajic
+
+// Date: September 15th, 2026
+
+// This program displays a greeting in proper style.
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Ms Raffin!\n";
+    std::cout << "This class is fun!\n";
+}
